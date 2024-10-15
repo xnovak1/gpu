8956b965bf29e23949257ccdf117c8692db2abb9
Michal Novák <xnovak9@fi.muni.cz>
2024-10-15 10:30:19 +0200

    split to 2 kernels

--- kernel.cu
+++ kernel.cu
@@ -1,18 +1,32 @@
 // divide into 2 kernels? 1 for account calculation and 1 for sum?
 
-__global__ void solve(int *changes, int *account, int *sum, int clients, int periods) {
+__global__ void calc_account(int *changes, int *account, int *sum, int clients, int periods) {
     // int x = blockIdx.x * blockDim.x + threadIdx.x;
     // int y = blockIdx.y * blockDim.y + threadIdx.y;
 
     int client_id = blockIdx.x * blockDim.x + threadIdx.x;
 
     if (client_id < clients) {
-        int accumulated_sum = 0;
+        int acc_sum = 0;
         for (int period = 0; period < periods; period++) {
-            int idx = day * clients + client_id; // Flattened index for (period, client)
-            accumulated_sum += changes[idx];
-            account[idx] = accumulated_sum;
+            int idx = period * clients + client_id; // Flattened index for (period, client)
+            acc_sum += changes[idx];
+            account[idx] = acc_sum;
         }
+    }
+}
+
+__global__ void calc_sum(int *account, int *sum, int clients, int periods) {
+    int period_id = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (period_id < periods) {
+        int acc_sum = 0;
+        for (int client = 0; client < clients; client++) {
+            int idx = period_id * periods + client;
+            acc_sum += account[idx];
+        }
+
+        sum[period_id] = acc_sum;
     }
 }
 
@@ -20,5 +34,9 @@ void solveGPU(int *changes, int *account, int *sum, int clients, int periods) {
     int BLOCK_SIZE = 16 * 16;
     int N_BLOCKS = (clients + BLOCK_SIZE - 1) / BLOCK_SIZE;
 
-    solve<<<N_BLOCKS, BLOCK_SIZE>>>(changes, account, sum, clients, periods);
+    calc_account<<<N_BLOCKS, BLOCK_SIZE>>>(changes, account, sum, clients, periods);
+
+    N_BLOCKS = periods; // (periods + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+    calc_sum<<<N_BLOCKS, BLOCK_SIZE>>>(account, sum, clients, periods);
 }
